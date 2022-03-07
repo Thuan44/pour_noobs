f8711f4524ae6c08613c8b3ad412d603e3bc4261
Cong-Thuan Do <congthuan274@gmail.com>
2022-03-07 22:11:09 +0100

    feat(FK): Category data added to Course on api call

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\UserController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CourseController;
 
 /*
@@ -16,18 +17,29 @@ use App\Http\Controllers\CourseController;
 |
 */
 
-/**
- * Public routes
- */
+/*
+|--------------------------------------------------------------------------
+| Public Routes
+|--------------------------------------------------------------------------
+*/
+// Authentication
 Route::post('register', [UserController::class, 'register'])->name('register');
 Route::post('login', [UserController::class, 'login'])->name('login');
+
+// Courses
 Route::get('courses', [CourseController::class, 'getCourses'])->name('getCourses');
 Route::get('courses/{id}', [CourseController::class, 'getCourseByID'])->name('getCourseByID');
 
+// Categories
+Route::get('categories', [CategoryController::class, 'getCategories'])->name('getCategories');
 
-/**
- * Protected routes
- */
+
+/*
+|--------------------------------------------------------------------------
+| Private Routes with Sanctum
+|--------------------------------------------------------------------------
+*/
+// Authentication
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('logout', [UserController::class, 'logout'])->name('logout');
 });
